27b0f4164d34bcd4f6ec033b9660948632f32979
thermo01 <hiranvinaayak04@gmail.com>
2024-07-20 21:40:25 +0800

    added testable trait, cleaned up clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,26 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
-
 mod vga_buffer;
 mod serial;
+
+use core::panic::PanicInfo;
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -31,7 +47,8 @@ pub extern "C" fn _start() -> ! {
 
     #[cfg(test)]
     test_main();
-
+    
+    #[allow(clippy::empty_loop)]
     loop {}
 }
 
@@ -53,19 +70,15 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
-
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
-    loop {}
 }
