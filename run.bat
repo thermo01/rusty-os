b147df28458bc4450632de7801e68639a6091791
thermo01 <hiranvinaayak04@gmail.com>
2024-07-21 11:39:53 +0800

    added build scripts

--- run.bat
+++ run.bat
@@ -1,6 +1,1 @@
 cargo build --release
-:: In my system, the `cargo run` command will give an error after building
-:: This may be because I was unable to set qemu to PATH, so this may apply to you to
-:: And `bootloader` execs a direct command to bin
-qemu-system-x86_64.exe -drive format=raw,file=target/x86_64-rusty_os/release/bootimage-rusty-os.bin
-
--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
 cargo build
-# Hopefully qemu runs for you
-#If not, please see docs
-qemu-system-x86_64.exe -drive format=raw,file=target/x86_64-rusty_os/debug/bootimage-rusty-os.bin
